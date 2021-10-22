f5d00e0ef02cf995746236166f713e928415ab7f
Jake <rizkall3@msu.edu>
2021-10-22 14:07:10 -0400

    Missing model page for PrivateSaveSearch

---
+++ app/Model/PrivateSaveSearch.php
@@ -0,0 +1,22 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class PrivateSaveSearch extends AppModel
+{
+    public $actsAs = array('AuditLog', 'Containable');
+
+    public $validate = array(
+        'value' => array(
+            'valueNotEmpty' => array(
+                'rule' => array('valueNotEmpty'),
+            ),
+        ),
+        'title' => array(
+                'valueNotEmpty' => array(
+                        'rule' => array('valueNotEmpty'),
+                ),
+        )
+    );
+
+    public $belongsTo = 'User';
+}
